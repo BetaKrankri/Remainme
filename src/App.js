50c4c71453a9462c7422174bf17a40e42a1ccd71
BetaKrankri <edominguezj11@outlook.com>
2023-03-27 14:58:29 -0600

    ListNav & TaskBoard layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
+import './auxStyles.scss';
+
 function App() {
   return (
     <div className="App">
 
+      <div className='ListNav'>
+        <div className='Body'>
+          <div className='ListButton'>
+            <div className='icon'></div>
+            <div className='label'>Nombre de lista</div>
+          </div>
+          <div className='ListButton'>
+            <div className='icon'></div>
+            <div className='label'>Nombre de lista</div>
+          </div>
+          <div className='ListButton'>
+            <div className='icon'></div>
+            <div className='label'>Nombre de lista</div>
+          </div>
+          <div className='ListButton'>
+            <div className='icon'></div>
+            <div className='label'>Nombre de lista</div>
+          </div>
+
+        </div>
+
+        <div className='Footer'>
+          <div className='NewTask'>
+            <div className='icon'></div>
+            <input className='' type='text' />
+          </div>
+        </div>
+      </div>
+
+      <div className='TaskBoard'>
+        <div className='Header'>
+          <div className='icon'></div>
+          <div className='label'>Nombre de lista</div>
+        </div>
+
+        <div className='Body'>
+          <div className='Task'>
+            <input type='checkbox' className='IsDone' />
+            <div className='Info'>
+              <div className='Title'>Titulo del Task</div>
+              <div className='Extra'></div>
+            </div>
+            <input className='IsStarred' type='checkbox' />
+          </div>
+
+          <div className='Task'>
+            <input type='checkbox' className='IsDone' />
+            <div className='Info'>
+              <div className='Title'>Titulo del Task</div>
+              <div className='Extra'></div>
+            </div>
+            <input className='IsStarred' type='checkbox' />
+          </div>
+        </div>
+
+        <div className='Footer'>
+          <div className='NewTask'>
+            <div className='icon'></div>
+            <input className='' type='text' />
+          </div>
+        </div>
+
+      </div>
+
+      {/* <div className='TaskDetails'></div> */}
     </div>
   );
 }
