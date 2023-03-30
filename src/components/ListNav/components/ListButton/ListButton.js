8a67b9c3ff4f0af9bba4ce49ad120936dbc214f6
BetaKrankri <edominguezj11@outlook.com>
2023-03-30 16:28:12 -0600

    ListNav buttons component

---
+++ src/components/ListNav/components/ListButton/ListButton.js
@@ -0,0 +1,10 @@
+function ListButton(props) {
+    return (
+        <div className='ListButton'>
+            <div className='icon'></div>
+            <div className='label'>Nombre de lista</div>
+        </div>
+    );
+}
+
+export default ListButton;
