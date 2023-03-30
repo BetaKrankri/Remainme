7536aa0cf1b8885e4eef0f988500bf75e0010778
BetaKrankri <edominguezj11@outlook.com>
2023-03-30 16:27:11 -0600

    NewList component

---
+++ src/components/ListNav/components/NewList/NewList.js
@@ -0,0 +1,10 @@
+function NewList(props) {
+    return (
+        <div className='NewList'>
+            <div className='icon'></div>
+            <input className='' type='text' />
+        </div>
+    );
+}
+
+export default NewList;
