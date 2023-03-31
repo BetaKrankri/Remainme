1a21345d0104428b74b729756a4d69673fb516e8
BetaKrankri <edominguezj11@outlook.com>
2023-03-31 13:34:15 -0600

    currentList onClick at ListButton

--- src/components/ListNav/ListButton/ListButton.js
+++ src/components/ListNav/ListButton/ListButton.js
@@ -1,6 +1,15 @@
-function ListButton({label}) {
+import { useContext } from "react";
+import { DataContext } from "../../../DataContext";
+
+function ListButton({ label }) {
+    const { setCurrentList } = useContext(DataContext);
+
     return (
-        <div className='ListButton'>
+        <div
+            className='ListButton'
+            onClick={() => {
+                setCurrentList(label)
+            }}>
             <div className='icon'></div>
             <div className='label'>{label}</div>
         </div>
