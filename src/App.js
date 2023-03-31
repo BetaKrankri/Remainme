7fd7290e317245d349e20d3f7d445371f1eb7f86
BetaKrankri <edominguezj11@outlook.com>
2023-03-31 13:29:41 -0600

    clean the app

--- src/App.js
+++ src/App.js
@@ -2,50 +2,20 @@ import { useState } from 'react';
 import './auxStyles.scss';
 import ListNav from './components/ListNav/ListNav';
 import TaskBoard from './components/TaskBoard/TaskBoard';
+import { DataProvider } from './DataContext';
+
+import fetchedData from './data.json';
 
 function App() {
-  const [data, setData] = useState([
-    {
-      listName: 'Tareas',
-      taskList: [{
-        title: 'Primer Rocordatorio',
-        isDone: false,
-        isStarred: true,
-        dateCreation: new Date(),
-      },
-      {
-        title: 'Segundo Rocordatorio',
-        isDone: true,
-        isStarred: true,
-        dateCreation: new Date(),
-      }]
-    }]);
-
-  function getListNames() {
-    // Regresa una arreglo con los nombres de las listas 
-    return data.map(list => list.listName);
-  }
-
-  function getTasksFromList(listName) {
-
-    const foundList = data.find(list => list.listName === listName);
-    return foundList ? foundList.taskList : [];
-  }
-
-  function addNewList(newListName) {
-    setData(prevState => [...prevState, {
-      listName: newListName,
-      taskList: []
-    }]);
-  }
-
 
   return (
-    <div className="App">
-      <ListNav listNames={getListNames()} />
-      <TaskBoard />
-      {/* <div className='TaskDetails'></div> */}
-    </div>
+    <DataProvider fetchedData={fetchedData} >
+      <div className="App">
+        <ListNav />
+        <TaskBoard />
+        {/* <div className='TaskDetails'></div> */}
+      </div>
+    </DataProvider>
   );
 }
 
