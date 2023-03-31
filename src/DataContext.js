60136fc8906b39662fae745d4dd37cd16b296644
BetaKrankri <edominguezj11@outlook.com>
2023-03-31 13:30:12 -0600

    add more context

--- src/DataContext.js
+++ src/DataContext.js
@@ -2,13 +2,14 @@ import React, { createContext, useState } from "react";
 
 export const DataContext = createContext();
 
-export function DataProvider({ children, fethchedData }) {
+export function DataProvider({ children, fetchedData }) {
 
-  const [data, setData] = useState(fethchedData);
+  const [data, setData] = useState(fetchedData);
+  const [currentList, setCurrentList] = useState('Tareas');
 
   return (
     <DataContext.Provider
-      value={{ data, setData }}
+      value={{ data, setData, currentList, setCurrentList }}
     >
       {children}
     </DataContext.Provider>
